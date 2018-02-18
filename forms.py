41afa4ed8e34ffd454cd6ae289bea86fe3c0e16a
Adam Zemmoura <adamzemmoura@gmail.com>
2018-02-18 23:12:20 +0100

    added login page

--- forms.py
+++ forms.py
@@ -8,3 +8,8 @@ class SignupForm(Form):
     email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
     password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be at least 6 characters long.")])
     submit = SubmitField('Sign up')
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+    password = StringField('Password', validators=[DataRequired("Please enter your password.")])
+    submit = SubmitField("Sign in")
